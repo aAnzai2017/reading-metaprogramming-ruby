049173892744d29ba83a815d7666704c1d017f80
Akihiro Anzai <akihiro.anzai@smarthr.co.jp>
2019-12-19 14:36:02 +0900

    テスト通した

--- 03_method/define.rb
+++ 03_method/define.rb
@@ -2,6 +2,12 @@
 # 次の動作をする A1 class を実装する
 # - "//" を返す "//"メソッドが存在すること
 
+class A1
+    define_method "//" do
+        "//"
+    end
+end
+
 # Q2.
 # 次の動作をする A2 class を実装する
 # - 1. "SmartHR Dev Team"と返すdev_teamメソッドが存在すること
@@ -10,7 +16,44 @@
 #   - 受け取った引数の回数分、メソッド名を繰り返した文字列を返すこと
 #   - 引数がnilの場合は、dev_teamメソッドを呼ぶこと
 
+class A2
+    def initialize(arr)
+        arr.each do |name|
+            A2.define_method "hoge_#{name}" do |arg|
+                if arg.nil?
+                    dev_team
+                else
+                    __method__.to_s * arg
+                end
+            end
+        end
+    end
+
+    def dev_team
+        "SmartHR Dev Team"
+    end
+end
+
 # Q3.
 # 次の動作をする OriginalAccessor モジュール を実装する
 # - OriginalAccessorモジュールはincludeされたときのみ、my_attr_accessorメソッドを定義すること
 # - my_attr_accessorはgetter/setterに加えて、boolean値を代入した際のみ真偽値判定を行うaccessorと同名の?メソッドができること
+
+module OriginalAccessor
+    def self.included(mod)
+        mod.define_singleton_method :my_attr_accessor do |name|
+            mod.define_method name do
+                instance_variable_get("@#{name}")
+            end
+
+            mod.define_method "#{name}=" do |arg|
+                instance_variable_set("@#{name}", arg)
+                if arg.instance_of?(TrueClass) || arg.instance_of?(FalseClass)
+                    define_singleton_method "#{name}?" do
+                        !!instance_variable_get("@#{name}")
+                    end
+                end
+            end
+        end
+    end
+end
