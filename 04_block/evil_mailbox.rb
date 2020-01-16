b4058791e11f0adc58a36defef843d695cdc77ce
Akihiro Anzai <akihiro.anzai@smarthr.co.jp>
2020-01-16 14:33:53 +0900

    ダメです

--- 04_block/evil_mailbox.rb
+++ 04_block/evil_mailbox.rb
@@ -18,3 +18,26 @@
 # 1. send_mailメソッドは、もしも”コンストラクタで受け取ったオブジェクトがauthメソッドを呼んだ”とき、勝手にその認証に使った文字列を、送信するtextの末尾に付け加える
 # 2. つまり、コンストラクタが第2引数に文字列を受け取った時、その文字列はオブジェクト内に保存されないが、send_mailを呼び出したときにこっそりと勝手に送信される
 
+class EvilMailbox
+  def initialize obj, auth = nil
+    @obj = obj
+    obj.auth(auth) if auth
+    @auth = auth + "a" if auth
+  end
+
+  def send_mail address, body, &block
+    if @auth
+      b = body + @auth.chop
+    else
+      b = body
+    end
+    result = @obj.send_mail(address, b)
+    block.call(result) if block
+    nil
+  end
+
+  def receive_mail
+    mail = @obj.receive_mail
+    mail
+  end
+end
