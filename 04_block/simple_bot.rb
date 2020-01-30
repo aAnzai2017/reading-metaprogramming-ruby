89100e892b87cdd1451f6f1a8bfd245344f2ae37
Akihiro Anzai <akihiro.anzai@smarthr.co.jp>
2020-01-30 14:58:55 +0900

    通した

--- 04_block/simple_bot.rb
+++ 04_block/simple_bot.rb
@@ -22,3 +22,54 @@
 # 4. クラスメソッドsettingは、引数を2つ取り、1つ目がキー名、2つ目が設定する値です
 #     1. settingメソッドに渡された値は、インスタンスメソッド `settings` から返されるオブジェクトに、メソッド名としてアクセスすることで取り出すことができます
 #     2. e.g. クラスメソッドで `setting :name, 'bot'` と実行した場合は、インスタンス内で `settings.name` の戻り値は `bot` の文字列になります
+
+require 'byebug'
+
+class SimpleBot
+    def self.respond key, &block
+        @@responds ||= {}
+        @@responds[key] = block
+    end
+
+    def self.setting key, value
+        @@settings ||= {}
+        @@settings[key] = value
+    end
+
+    def initialize
+        @obj = Object.new
+        s = @@settings
+        @obj.instance_eval do
+            define_singleton_method :method_missing do |key|
+                if s.include? key
+                    s[key]
+                else
+                    nil
+                end
+            end
+        end
+    end
+
+    def ask word
+        if @@responds.include? word
+            instance_eval &@@responds[word]
+        else
+            nil
+        end
+    end
+
+    def settings
+        @obj
+    end
+end
+
+
+class Bot < SimpleBot
+  setting :name, 'bot'
+  respond 'keyword' do
+    "response #{settings.name}"
+    # 'hello'
+  end
+end
+
+puts Bot.new.ask('keyword')
